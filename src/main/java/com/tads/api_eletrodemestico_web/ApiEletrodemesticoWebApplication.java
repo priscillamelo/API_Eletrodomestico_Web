30e51b41aa229d2f0657e0413ec761580b7c221c
priscilla-melo <priscillamelo73@gmail.com>
2022-07-18 22:40:46 -0300

    projeto api web - inicio

---
+++ src/main/java/com/tads/api_eletrodemestico_web/ApiEletrodemesticoWebApplication.java
@@ -0,0 +1,13 @@
+package com.tads.api_eletrodemestico_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEletrodemesticoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiEletrodemesticoWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tads/api_eletrodemestico_web/ApiEletrodemesticoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tads.api_eletrodemestico_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEletrodemesticoWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
